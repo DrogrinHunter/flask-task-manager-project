2eb072fe313947f98c9a59293521c15b2c2a3689
DrogrinHunter <lukewillmore67@gmail.com>
2021-09-29 19:38:34 +0000

    Add session user profile

--- app.py
+++ app.py
@@ -47,6 +47,8 @@ def register():
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful")
+        return redirect(url_for("profile", username=session["user"]))
+
     return render_template("register.html")
 
 
@@ -62,15 +64,26 @@ def login():
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
+                    flash("Welcome, {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
                 return redirect(url_for("login"))
         else:
             # username doesn't exist
-            flash("Incorrect Username and/or Password")        
+            flash("Incorrect Username and/or Password")
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grab the session user's username from the db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
@@ -78,3 +91,4 @@ if __name__ == "__main__":
             port=int(os.environ.get("PORT")),
             debug=True)
 # turn debug to false before submitting
+
